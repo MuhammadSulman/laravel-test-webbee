7cf89809e32a4b3445d6180724c66dbb49584730
Muhammad Sulman <sulman@users-MacBook-Pro.local>
2023-06-18 15:52:22 +0500

    implemented getEventsWithWorkShops

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,7 +4,37 @@ namespace App\Models;
 
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Event extends Model
 {
+    /**
+     * @var string
+     */
+    protected $table = "events";
+
+    /**
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name'
+    ];
+
+    /**
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    public function workshops(): HasMany
+    {
+        return $this
+            ->hasMany(
+                Workshop::class,
+                'event_id',
+                'id'
+            );
+    }
 }
